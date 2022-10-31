5928b1ec85e0f5bb1a63726391a7bec7e844566b
Rayen Ben Driaa <rayenbd63s@gmail.com>
2022-10-31 16:58:14 +0100

    implemented the icons in the external tiles

--- lib/brief/brief_info.dart
+++ lib/brief/brief_info.dart
@@ -3,17 +3,52 @@ import 'package:flutter/material.dart';
 
 import 'brief_detail.dart';
 
-class BriefInfo extends StatelessWidget {
+class BriefInfo extends StatefulWidget {
   final String _title;
   final String _content;
   final String _description;
   final int _indicator;
   final String _image;
-  
-
-
   BriefInfo(
       this._image, this._title, this._content, this._description, this._indicator);
+
+  @override
+  State<BriefInfo> createState() => _BriefInfoState();
+}
+
+class _BriefInfoState extends State<BriefInfo> {
+  var varicon = Symicons.Icons_297;
+
+  String s ="Symicons.Icons_297";
+
+    @override
+  void initState() {
+    _icons();  
+    
+    super.initState();
+  }
+
+  IconData _icons(){
+    switch (widget._indicator){
+      case 1 : 
+      return varicon = Symicons.Icons_hamburger_menu_icon_v2_120x120;
+      case 2: 
+      return varicon = Symicons.Icons_dialog_icon_120x120;
+      case 3 :
+      return varicon = Symicons.Icons_xls_file_icon_120x120;
+      case 4:
+      return varicon = Symicons.Icons_files_icon_120x120;
+      case 0 :
+      return varicon = Symicons.Icons_add_users_icon_120x120;
+      default: 
+      return varicon = Symicons.Icons_146;
+
+
+      
+      
+    }
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +58,7 @@ class BriefInfo extends StatelessWidget {
           onTap: () {
             Navigator.of(context)
                 .push(MaterialPageRoute(builder: (BuildContext context) {
-              return BriefDetails(_title, _content, _description, _indicator);
+              return BriefDetails(widget._title, widget._content, widget._description, widget._indicator);
             }));
           },
           child: Stack(
@@ -31,26 +66,26 @@ class BriefInfo extends StatelessWidget {
               SizedBox(
                 width: 1500,
                 child: Image.asset(
-                  _image,
+                  widget._image,
                   fit: BoxFit.fill,
                 ),
               ),
-                const SizedBox(
+                SizedBox(
                 width: 1500,
                 height: 500,
-                child: Icon(Symicons.Icons_297)
+                child: Icon(varicon,color: Colors.white,)
               ),
               Container(
                 margin: const EdgeInsets.only(top: 70, left: 25, right: 20),
                 child: Column(
                   children: [
-                    Center(child: Text(_title, textScaleFactor: 2)),
+                    Center(child: Text(widget._title, textScaleFactor: 2)),
                     const SizedBox(
                       height: 10,
                     ),
                     Center(
                         child: Text(
-                      _description,
+                      widget._description,
                       style: const TextStyle(fontStyle: FontStyle.italic),
                     )),
                   ],
