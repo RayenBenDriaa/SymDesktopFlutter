d6f78050f83bdcd705049241f18955ed5856d8c5
Rayen Ben Driaa <rayenbd63s@gmail.com>
2022-11-04 16:42:39 +0100

    integrated app bar which was done by mahdi

--- lib/brief/briefMain.dart
+++ lib/brief/briefMain.dart
@@ -1,3 +1,4 @@
+import 'package:briefcase/icons/symicons.dart';
 import 'package:flutter/material.dart';
 import 'briefCase.dart';
 import 'package:no_context_navigation/no_context_navigation.dart';
@@ -5,9 +6,9 @@ import 'package:no_context_navigation/no_context_navigation.dart';
 const symColor = Color(0xFF1e4cab);
 
 final NavigationService navService = NavigationService();
-class GlobalContextService { 
-  static GlobalKey<NavigatorState> navigatorKey = 
-  GlobalKey<NavigatorState>();
+
+class GlobalContextService {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 }
 
 class BriefMain extends StatefulWidget {
@@ -17,10 +18,16 @@ class BriefMain extends StatefulWidget {
   State<BriefMain> createState() => _BriefMain();
 }
 
-
 class _BriefMain extends State<BriefMain> {
+  String? dropdownValue;
   bool shadowColor = false;
+  var _popupMenuItemIndex = 0;
   double? scrolledUnderElevation;
+  _onMenuItemSelected(int value) {
+    setState(() {
+      _popupMenuItemIndex = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +35,117 @@ class _BriefMain extends State<BriefMain> {
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(70.0), // here the desired height
         child: AppBar(
-          actions: [
-            IconButton(
-              color: Colors.white,
-              onPressed: () {
-                debugPrint("hello");
-              },
-              icon: const Icon(
-                Icons.info_outline,
-              ),
-            )
-          ],
-          title: const Text(
-            'SYMMETRYK',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+         actions: [
+          Row(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Symicons.Icons_small_bottom_arrow_icon_120x120,color: Colors.white,),
+                    elevation: 16,
+                    style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0),),
+                    underline: Container(
+                      height: 0,
+
+                      color: Color.fromARGB(255, 35, 13, 96),
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                    items: <String>[
+                      'Choose your briefcase',
+                      'Briefcase init',
+                      'Briefcase 1',
+                      'Briefcase 2'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Symicons.Icons_download_icon_120x120),
+                    style: IconButton.styleFrom(foregroundColor: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Symicons.Icons_survey_icon_120x120),
+                    style: IconButton.styleFrom(foregroundColor: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon:
+                          const Icon(Symicons.Icons_tag_list_icon_120x120),
+                      style: IconButton.styleFrom(foregroundColor: Colors.white)),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Symicons.Icons_favorites_list_icon_120x120),
+                    style: IconButton.styleFrom(foregroundColor: Colors.white)
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                   child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Symicons.Icons_config_icon_120x120),
+                    style: IconButton.styleFrom(foregroundColor: Colors.white)
+                                 ),
+                 ),
+
+              // PopupMenuButton(
+              //   icon: Icon(Icons.more_vert),
+              //   itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              //     const PopupMenuItem(
+              //       child: ListTile(
+              //         leading: Icon(Icons.add),
+              //         title: Text('Item 1'),
+              //       ),
+              //     ),
+              //     const PopupMenuItem(
+              //       child: ListTile(
+              //         leading: Icon(Icons.anchor),
+              //         title: Text('Item 2'),
+              //       ),
+              //     ),
+              //     const PopupMenuItem(
+              //       child: ListTile(
+              //         leading: Icon(Icons.article),
+              //         title: Text('Item 3'),
+              //       ),
+              //     ),
+              //     const PopupMenuDivider(),
+              //     const PopupMenuItem(child: Text('Item A')),
+              //     const PopupMenuItem(child: Text('Item B')),
+              //   ],
+              // ),
+
+             
+            ],
           ),
+        ],
+          title:   IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Symicons.Icons_search_icon_120x120),
+                  style: IconButton.styleFrom(foregroundColor: Colors.white)
+                ),
           leading: InkWell(
             child: Image.asset(
               "assets/images/symmetryk.png",
